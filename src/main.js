503ee6c56ceca403f7a7c7352db316435f01b40e
hjin125 <https://github.com/hjin111>
2024-08-06 18:15:26 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// main.js 는 vue 애플리케이션의 시작점이다.
+
+import { createApp } from 'vue'
+import App from './App.vue'
+// src/router/index.js 파일의 router를 사용하겠다는 선언
+import router from '@/router/index.js'
+import vuetify from '@/plugins/vuetify';
+// createApp(App).mount('#app')
+
+const app = createApp(App);
+
+
+app.use(router);
+app.use(vuetify);
+
+app.mount('#app');
---
+++ src/views/FlintView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+    <div class="image-container">
+        <img src="@/assets/image.png" alt="background image">
+    </div>
+    </div>
+    
+</template>
+
+<script>
+import HeaderComponent from '../components/HeaderComponent.vue';
+
+export default{
+   components(){
+    HeaderComponent
+   }
+}
+console.log("hi");
+</script>
+
+<style>
+.image-container {
+    width: 100%;
+    height: 100%;
+  }
+  
+  .image-container img {
+    width: 1519.2px;
+    height: 740px;
+    position: relative;
+    z-index: 8;
+  }
+</style>
