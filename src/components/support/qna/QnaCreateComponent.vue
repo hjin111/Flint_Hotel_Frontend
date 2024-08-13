bb636eadbdb21f8a6c8f3874e949e87a9b727aee
H-Yeji <yeji980603@naver.com>
2024-08-13 17:47:08 +0900

    Feat: qna작성 틀 생성

---
+++ src/components/support/qna/QnaCreateComponent.vue
@@ -0,0 +1,88 @@
+<template>
+    <QnaView />
+    <v-container class="qna-container">
+        <br>
+        <h3 style="text-align:center;">질문하기</h3>
+        <br>
+        <v-row>
+            <v-col>
+                <v-card class="qna-card">
+                    
+                </v-card>
+            </v-col>
+        </v-row>
+     </v-container> 
+  </template>
+  
+  <script>
+  import QnaView from '@/views/QnaView.vue';
+import axios from 'axios';
+  export default {
+    components: {
+        QnaView
+    },
+    data() {
+        return {
+            qnaList: [],
+            tableHeaders: [
+                {title:'No', key:'no', align:'start'},
+                {title:'Title', key:'title', align:'start'},
+                {title:'Email', key:'memberEmail', align:'start'},
+                {title: 'Write time', key: 'writeTime', align: 'start'},
+                {title:'Modify', align:'start'}
+            ],
+        }
+    },
+    created() {
+        this.loadList();
+    },
+    methods: {
+        async loadList() {
+            try {
+                const token = localStorage.getItem('membertoken');
+                // {headers: {Authorization: 'Bearer 토큰 값'}}}
+                const headers = {Authorization: `Bearer ${token}`};
+                console.log(headers);
+                const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/mypage/qna/list`, {headers});
+
+                console.log('Response: ', response.data);
+                this.qnaList = response.data; // setting 
+            } catch(e) {
+                console.log('Error: ', e);
+            }
+        },
+        formatDate(dateString) {
+            if (!dateString) return '';
+
+            const [datePart, timePart] = dateString.split('T');
+            return `${datePart} ${timePart}`;
+        },  
+    }
+    
+  }
+  </script>
+  
+  <style scoped>
+  .custom-title {
+    font-family: "Playfair Display", serif;
+    color: #787878;
+    font-size:20px;
+    border-bottom: 3px solid #787878;
+    text-align: center;
+  }
+  .qna-container {
+    background-color: white;
+    position: absolute;
+    width: 90%;
+    max-width: 1200px;
+    height: 80%;
+    top: 57%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 10px;
+    overflow-y: auto;
+  }
+  .qna-card {
+    padding: 20px;
+  }
+  </style>
--- src/router/qnaRouter.js
+++ src/router/qnaRouter.js
@@ -1,10 +1,17 @@
 import QnaListComponent from "@/components/support/qna/QnaListComponent.vue";
+import QnaCreateComponent from "@/components/support/qna/QnaCreateComponent.vue";
 
 export const qnaRouter = [
     {
         path: '/mypage/qna/list', 
         name: 'QnaListComponent',
         component: QnaListComponent,
+        meta : {header: 'HeaderComponent'}
+    },
+    {
+        path: '/mypage/qna/create', 
+        name: 'QnaCreateComponent',
+        component: QnaCreateComponent,
         meta : {header: 'HeaderComponent'}
     }
 ]
