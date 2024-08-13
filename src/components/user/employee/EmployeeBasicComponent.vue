e44efad0d7aaa3262539b058943089844bb4b65e
jasonhwang <junghahwang0601@gmail.com>
2024-08-13 17:54:06 +0900

    Fix: 직원 페이지 배경 추가

---
+++ src/components/user/employee/EmployeeBasicComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <EmployeeView />
+    <v-container class="content-container">
+    </v-container>
+  </template>
+    
+    <script>
+    import EmployeeView from '@/views/EmployeeView.vue';
+  
+    export default {
+      components: {
+        EmployeeView
+      },
+      data() {
+        return {
+        };
+      }
+    };
+    </script>
+    
+    <style scoped>
+    .content-container {
+        background-color: white;
+        position: absolute;
+        width: 90%;
+        max-width: 1200px;
+        height: 80%;
+        top: 55%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        border-radius: 10px;
+        overflow-y: auto;
+        flex-direction: column;
+        padding-left:40px;
+        padding-right:40px;
+      }
+    </style>
